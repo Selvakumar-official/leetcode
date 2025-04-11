5fea7fb45d169327160b776c25c67c9455e5a23e
selvakumar1190 <109788803+selvakumar1190@users.noreply.github.com>
2025-04-11 14:13:05 +0530

    Time: 132 ms (5.55%), Space: 46.2 MB (7.36%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s){
+        s=s.toLowerCase();
+        String sl= "";
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                sl+=s.charAt(i);
+            }
+        }
+        return palin(sl);
+    }
+    public static boolean palin(String sl){
+        StringBuilder sb= new StringBuilder(sl);
+        // String rev = sb.reverse().toString
+        return sl.equals(sb.reverse().toString());
+    }
+}
