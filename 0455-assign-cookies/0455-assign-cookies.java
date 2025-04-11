8a2ba0da9101e16692194a296157eeb0bda2f4c3
selvakumar1190 <109788803+selvakumar1190@users.noreply.github.com>
2025-04-11 10:40:07 +0530

    Time: 11 ms (39.79%), Space: 48 MB (98.19%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        if(s.length==0){
+            return 0;
+        }
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0,j=0;
+        while(i<g.length&& j<s.length){{
+            if(s[j]>=g[i]){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+
+        }
+        return i;
+    }
+}
